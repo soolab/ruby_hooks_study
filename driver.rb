e0fa3b6ca73db62e9b975a18c0a8110e559bda55
kimjinsoo <kimjs@dramancompany.com>
2021-11-14 15:10:00 +0900

    define class with in different file location

---
+++ driver.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+# driver calss for testing
+require_relative './person'
+class Driver
+  # from splitted person module
+  include Person
+end
+
+driver_one = Driver.new
+
+driver_one.print_hello_world
+Person.self_print_hello_worlda
