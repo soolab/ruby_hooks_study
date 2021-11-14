37f207535df4b015f056f8660faa72b72ff2bcf2
kimjinsoo <kimjs@dramancompany.com>
2021-11-14 16:52:30 +0900

    understanding self.prepended

--- prepend_module.rb
+++ prepend_module.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
-
+# prepend module for testing
 module PrependModule
+
+  def self.prepended(base)
+    puts "#{base} prepended #{self}"
+  end
+
   def hello_world
     puts 'hello world from prepend module'
   end
--- prepend_module_adopt.rb
+++ prepend_module_adopt.rb
@@ -7,6 +7,11 @@ class PrependModuleAdopt
   # this method is overrided by prepend module
   # because prepend module's priority is higher than
   # PrependModuleAdopt's hello_world instance method
+
+  # module's prepend keyword is like include
+  # but the only differece is that
+  # prepend modules method can overried class method
+  # if they have same method name
   def hello_world
     puts 'hello world from class instance method'
   end
